b2303f4a80ce1a118bc666edf6a7d864d45e3e67
maxleclerc2 <79794192+maxleclerc2@users.noreply.github.com>
2021-04-27 18:40:23 +0200

    Modification du style du menu

--- Accueil.php
+++ Accueil.php
@@ -36,37 +36,37 @@
             <li class='NavAccueil'>
             <a href='Accueil.php'>Accueil</a>
             </li>
-            <li class='NavAccueil'>
+            <li class='NavBoutique'>
             <a href='Boutique.php'>Tous nos</br>produits</a>
             </li>";
 
             if($res->num_rows > 0) {
                 while($row = $res->fetch_assoc()) {
-                    echo "<li class='NavParcours'>";
+                    echo "<li class='NavBoutique'>";
                     echo "<a href='Boutique.php?category=" . $row["Slug_Categorie"] . "'>" . $row["Titre_Categorie"] . "</a>";
                     echo "</li>";
                 }
             }
 
-            echo "<li class='NavAccueil'>";
+            echo "<li class='NavPanier'>";
             echo "<a href='Panier.php'>Mon panier</a>";
             echo "</li>";
 
             if(isset($_SESSION["admin"]) && $_SESSION["admin"] == 1) {
-                echo "<li class='NavParcours'>";
+                echo "<li class='NavAdmin'>";
                 echo "<a href='Administration.php'>Espace</br>administrateur</a>";
                 echo "</li>";
             }
 
             if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] == true) {
-                echo "<li class='NavParcours'>";
+                echo "<li class='NavUtilisateur'>";
                 echo "<a href='Compte.php'>Mon compte</a>";
                 echo "</li>";
-                echo "<li class='NavParcours'>";
+                echo "<li class='NavUtilisateur'>";
                 echo "<a href='Deconnexion.php'>Déconnexion</a>";
                 echo "</li>";
             } else {
-                echo "<li class='NavParcours'>";
+                echo "<li class='NavUtilisateur'>";
                 echo "<a href='Connexion.php'>Connexion</a>";
                 echo "</li>";
             }
